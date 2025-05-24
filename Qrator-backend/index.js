7ffdbc3629e9a4d46ab41072ae8bf5c64a1a5bcc
Ridaa <ridaa.24bcs10394@sst.scaler.com>
2025-05-25 04:39:19 +0530

    created dashboard

--- Qrator-backend/index.js
+++ Qrator-backend/index.js
@@ -3,12 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import generateRoutes from './routes/generate.js';
 import scriptRoutes from './routes/script.js';
+import dashboardRoutes from './routes/dashboard.js';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/generate', generateRoutes);
 app.use('/script',scriptRoutes);
+app.use('/dashboard', dashboardRoutes);
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT,()=>{
