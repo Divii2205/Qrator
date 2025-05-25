02ed299d380c75a932a6f5fd974c813d8c0af5dd
Divii2205 <divijaa.24bcs10206@sst.scaler.com>
2025-05-25 14:39:56 +0530

    final commit

--- Qrator/src/main.jsx
+++ Qrator/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Landing from "./pages/Landing.jsx";
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import Idea from "./pages/Idea.jsx";
 import Script from "./pages/Script.jsx";
 import SEO from "./pages/SEO.jsx";
